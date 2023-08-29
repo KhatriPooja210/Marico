68344b253e0c16a1424ffe4787fbf19afe9ac28a
KhatriPooja210 <khatripooja210@gmail.com>
2023-08-29 10:59:35 +0530

    sixth commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { BiChevronDown } from "react-icons/bi";
+import { CgMenu } from "react-icons/cg";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
       <nav className="nav">
@@ -15,18 +22,18 @@ const Navbar = () => {
         <div>
           <ul className="navbar-list">
             <li>
-              <NavLink to="/">
+              <NavLink to="/" activeClassName="active">
                 Use Cases <BiChevronDown />
               </NavLink>
             </li>
             <li>
-              <NavLink to="/about">About</NavLink>
+              <NavLink to="/about" activeClassName="active">About</NavLink>
             </li>
             <li>
-              <NavLink to="/pricing">Pricing</NavLink>
+              <NavLink to="/pricing" activeClassName="active">Pricing</NavLink>
             </li>
             <li>
-              <NavLink to="/blog">Blog</NavLink>
+              <NavLink to="/blog" activeClassName="active">Blog</NavLink>
             </li>
           </ul>
         </div>
@@ -38,7 +45,37 @@ const Navbar = () => {
             <NavLink to="/signUp">Sign Up</NavLink>
           </button>
         </div>
+        <div className="mobile-menu-icon" onClick={handleToggle}>
+          <CgMenu />
+        </div>
       </nav>
+
+      {isOpen && (
+        <div className="mobile-menu overlay">
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName="active">Use Cases <BiChevronDown /></NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="active">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/pricing" activeClassName="active">Pricing</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog" activeClassName="active">Blog</NavLink>
+            </li>
+          </ul>
+          <div>
+          <button className="navBtn">
+            <NavLink to="/login">Login</NavLink>
+          </button>
+          <button className="navBtn">
+            <NavLink to="/signUp">Sign Up</NavLink>
+          </button>
+        </div>
+        </div>
+      )}
     </header>
   );
 };
